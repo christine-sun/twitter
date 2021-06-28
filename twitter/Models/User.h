a41f7703816ebd2ae34d4f6a22789cf059b558cf
Christine <christinesunnjm@gmail.com>
2021-06-28 18:06:37 -0400

    created user and tweet data models

---
+++ twitter/Models/User.h
@@ -0,0 +1,25 @@
+//
+//  User.h
+//  twitter
+//
+//  Created by Christine Sun on 6/28/21.
+//  Copyright © 2021 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+// MARK: Properties
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *screenName;
+@property (nonatomic, strong) NSString *profilePicture;
+
+// MARK: Methods
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
