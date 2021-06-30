db24553b1a33d771291595bb97881d8a8e11d915
Christine <christinesunnjm@gmail.com>
2021-06-30 15:39:53 -0400

    added dateTool for time ago

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -14,6 +14,7 @@
 #import "UIImageView+AFNetworking.h"
 #import <QuartzCore/QuartzCore.h>
 #import "ComposeViewController.h"
+#import "DateTools.h"
 
 @interface TimelineViewController () <ComposeViewControllerDelegate, UITableViewDelegate, UITableViewDataSource>
 
@@ -90,8 +91,8 @@
     cell.tweet = tweet;
     cell.nameLabel.text = tweet.user.name;
     cell.screennameLabel.text = [@"@" stringByAppendingString:tweet.user.screenName];
-    cell.dateLabel.text = tweet.createdAtString;
     cell.tweetTextLabel.text = tweet.text;
+    cell.dateLabel.text = [tweet.date shortTimeAgoSinceNow];
 
     // Configure favorites button
     [cell.favoritesButton
