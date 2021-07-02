4f0e70328787307d0db4279b91842d34fd6f0aab
Christine <christinesunnjm@gmail.com>
2021-07-01 20:37:44 -0400

    final submission

--- twitter/View Controllers/ComposeViewController.m
+++ twitter/View Controllers/ComposeViewController.m
@@ -36,23 +36,13 @@
         postStatusWithText:self.tweetTextView.text
         completion:^(Tweet *tweet, NSError *error) {
             if (tweet) {
-                NSLog(@"Successfully composed tweet");
+                // NSLog(@"Successfully composed tweet");
                 [self.delegate didTweet:tweet];
                 [self dismissViewControllerAnimated:true completion:nil];
             } else {
-                NSLog(@"Error composing tweet: %@", error.localizedDescription);
+                // NSLog(@"Error composing tweet: %@", error.localizedDescription);
             }
     }];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- twitter/View Controllers/ProfileViewController.m
+++ twitter/View Controllers/ProfileViewController.m
@@ -43,14 +43,4 @@
     self.followersLabel.text = [followersString stringByAppendingString:@" followers"];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
